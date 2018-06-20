438c8857ab43fc12b2382ffb4324b971d2cc19b8
Douglas Taquary <douglas.taquary@resourceit.com>
2018-06-20 19:02:26 -0300

    Update new fuctions

--- Helpers.py
+++ Helpers.py
@@ -4,22 +4,39 @@ import os
 def showMenu():
     return input("\nOlá, bem vindo!\n\nO que deseja fazer?\n\n" +
 "<C> - Criar novo usuário\n" +
-"<B> - Buscar usuário\n" +
+"<B> - Buscar usuário por nome\n" +
 "<E> - Excluir usuário\n" +
 "<D> - Informaçoes do usuário\n").upper()
 
-def addUser(user):
+def addUser(user, arquivo):
     user[input("Digite o login: ").upper()] = [input("Digite o nome completo: ").upper(),
                                                      input("Digite o cargo:  "),
                                                      input("Digite a data de acesso:  "),
                                                      input("Qual o nível de acesso desse usuário: ").upper()]
-    saveUser(user)
+    dicionario = lerArquivo(arquivo)
+    for key, value in dicionario.items():
+        if user == value:
+            print("Já existe usuário com esse login! Tente outro nome!")
+            showMenu()
 
-def saveUser(user):
-    with open("users.json", "w") as json_file:
-            json.dump(user, json_file)
+        saveUser(user, arquivo)
+        print("\nNovo usuário adicionado! ")
 
-def searchPerName(name):
+
+def saveUser(user, arquivo):
+    with open(arquivo, "w") as users:
+            json.dump(user, users)
+
+
+def lerArquivo(arquivo):
+    if os.path.exists(arquivo):
+        with open(arquivo, "r") as users:
+            users=json.load(users)
+    else:
+        users = {}
+    return users
+
+def searchPerName(name, arquivo):
     if os.path.exists(name):
         with open(name, "r") as users:
             user=json.load(users)
@@ -27,3 +44,14 @@ def searchPerName(name):
         user = {}
     return user
 
+def exibirDetalhesPorNome(login, arquivo):
+    dicionario = lerArquivo(arquivo)
+    for key, value in dicionario.items():
+        if login == value:
+            print("Nome reduzido/login...: ", value)
+            print("Nome completo.........: ", value[0])
+            print("Cargo.................: ", value[1])
+            print("Ultimo acesso.........: ", value[2])
+            print("Função:...............: ", value[3])
+        else:
+            print("Nenhum usuário foi encontrado!")
--- UsersControl.py
+++ UsersControl.py
@@ -2,9 +2,12 @@ from capitulo5.Helpers import *
 
 usuarios={}
 opcao = showMenu()
+arquivo = "users.json"
 
 while opcao == "C" or opcao == "B" or opcao == "E" or opcao == "D":
     if opcao == "C":
-        saveUser(usuarios)
+        addUser(usuarios, arquivo)
+    elif opcao == "B":
+        exibirDetalhesPorNome(input("Digite o login do usuário que deseja pesquisar: ").upper(), arquivo)
 
     opcao = showMenu()
